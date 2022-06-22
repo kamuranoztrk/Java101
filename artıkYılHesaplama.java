2dc8078661ead5f864cc846689c96e436f73cbe1
Kamuran ÖZTÜRK <105446944+kamuranoztrk@users.noreply.github.com>
2022-06-23 01:15:51 +0300

    Add files via upload

---
+++ artıkYılHesaplama.java
@@ -0,0 +1,17 @@
+package Giriş_2;
+import java.util.Scanner;
+public class artıkYılHesaplama {
+    public static void main(String[] args) {
+        int yıl;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Yıl giriniz: ");
+        yıl = input.nextInt();
+
+        if (yıl % 4 == 0 && yıl % 400 == 0){
+            System.out.println(yıl + " bir artık yıldır!");
+        }else {
+            System.out.println(yıl + "bir artık yıl değildir!");
+        }
+    }
+}
