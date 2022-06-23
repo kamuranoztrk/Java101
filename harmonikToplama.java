c47a8c368ec41a466a1aa77db11d8a0f4f121c0d
Kamuran ÖZTÜRK <105446944+kamuranoztrk@users.noreply.github.com>
2022-06-23 19:40:21 +0300

    Add files via upload

---
+++ harmonikToplama.java
@@ -0,0 +1,15 @@
+package Giriş_3;
+import java.util.Scanner;
+public class harmonikToplama {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("N sayısını giriniz: ");
+        int n = input.nextInt();
+        double result = 0;
+
+        for (double i = 1; i <= n; i++) {
+            result += (1 / i);
+        }
+        System.out.println("Harmonik toplam: " + result);
+    }
+}
